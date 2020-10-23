df2962fc6616038d148cfc2503bd24b7344ab4bf
Calvin Tran <trandcanh@gmail.com>
2020-10-24 00:08:08 +0800

    add dependencies to setup

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ setup(
     author_email='',
     license='BSD 2-clause',
     packages=['ezalor'],
-    install_requires=['numpy'],
+    install_requires=[
+        'numpy',
+        'pyhive',
+        'pandas'
+    ],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',  
         'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
